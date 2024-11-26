e03a9d7524d36b99f2425202ac2d59c491e6b126
Paolo Mazzon <pdmazzon@gmail.com>
2024-11-26 12:35:02 -0800

    Implemented the parts of the Steam API relavent to the project

--- src/SteamBindings.cpp
+++ src/SteamBindings.cpp
@@ -29,6 +29,13 @@ void vksk_SteamQuit() {
 	gSteamAPIInit = false;
 }
 
+// isInitialized -> bool
+void vksk_RuntimeSteamIsInitialized(WrenVM* vm) {
+	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
+	wrenSetSlotBool(vm, 0, gSteamAPIInit);
+}
+
+// getAchievement(name) -> bool
 void vksk_RuntimeSteamGetAchievement(WrenVM* vm) {
 	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_STRING, FOREIGN_END)
 	bool achieved = false;
@@ -37,53 +44,119 @@ void vksk_RuntimeSteamGetAchievement(WrenVM* vm) {
 	wrenSetSlotBool(vm, 0, achieved);
 }
 
+// getAchievementAchievedPercent(name) -> float
 void vksk_RuntimeSteamGetAchievementAchievedPercent(WrenVM* vm) {
 	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_STRING, FOREIGN_END)
 	float achievedPercent = 1.0f;
 	if (gSteamAPIInit)
-		SteamUserStats()->GetAchievement(wrenGetSlotString(vm, 1), &achievedPercent);
+		SteamUserStats()->GetAchievementAchievedPercent(wrenGetSlotString(vm, 1), &achievedPercent);
 	wrenSetSlotDouble(vm, 0, achievedPercent);
 }
 
+// getAchievementAndUnlockTime(name) -> [bool, string]
 void vksk_RuntimeSteamGetAchievementAndUnlockTime(WrenVM* vm) {
-	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
-
+	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_STRING, FOREIGN_END)
+	bool achieved = false;
+	uint32_t seconds = 0;
+	char *str;
+	if (gSteamAPIInit)
+		SteamUserStats()->GetAchievementAndUnlockTime(wrenGetSlotString(vm, 1), &achieved, &seconds);
+	str = ctime((const time_t*)&seconds);
+	wrenSetSlotNewList(vm, 0);
+	wrenSetSlotBool(vm, 1, achieved);
+	wrenInsertInList(vm, 0, -1, 1);
+	wrenSetSlotString(vm, 1, str);
+	wrenInsertInList(vm, 0, -1, 1);
 }
 
-void vksk_RuntimeSteamGetAchievementDisplayAttribute(WrenVM* vm) {
-	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
+// getAchievementDisplayAttributes(name) -> {"name": string, "desc": string, "hidden": bool, "unlocked": bool}
+void vksk_RuntimeSteamGetAchievementDisplayAttributes(WrenVM* vm) {
+	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_STRING, FOREIGN_END)
+	char *string;
+	const char *pchName = wrenGetSlotString(vm, 1);
+	const int keySlot = 1;
+	const int valueSlot = 2;
+	const int mapSlot = 0;
+	bool achieved;
+	wrenEnsureSlots(vm, 3);
+	wrenSetSlotNewMap(vm, mapSlot);
 
+	// Add the achievement attributes to a Wren map
+	if (gSteamAPIInit) {
+		wrenSetSlotString(vm, keySlot, "name");
+		wrenSetSlotString(vm, valueSlot, SteamUserStats()->GetAchievementDisplayAttribute(pchName, "name"));
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+
+		wrenSetSlotString(vm, keySlot, "desc");
+		wrenSetSlotString(vm, valueSlot, SteamUserStats()->GetAchievementDisplayAttribute(pchName, "desc"));
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+
+		wrenSetSlotString(vm, keySlot, "hidden");
+		wrenSetSlotBool(vm, valueSlot, strcmp(SteamUserStats()->GetAchievementDisplayAttribute(pchName, "hidden"), "1") == 0);
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+		
+		wrenSetSlotString(vm, keySlot, "unlocked");
+		SteamUserStats()->GetAchievement(pchName, &achieved);
+		wrenSetSlotBool(vm, valueSlot, achieved);
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+	} else {
+		// Default values if the API is not initialized
+		wrenSetSlotString(vm, keySlot, "name");
+		wrenSetSlotString(vm, valueSlot, "");
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+
+		wrenSetSlotString(vm, keySlot, "desc");
+		wrenSetSlotString(vm, valueSlot, "");
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+
+		wrenSetSlotString(vm, keySlot, "hidden");
+		wrenSetSlotBool(vm, valueSlot, false);
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+
+		wrenSetSlotString(vm, keySlot, "unlocked");
+		wrenSetSlotBool(vm, valueSlot, false);
+		wrenSetMapValue(vm, mapSlot, keySlot, valueSlot);
+	}
 }
 
-void vksk_RuntimeSteamGetAchievementName(WrenVM* vm) {
-	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
-
-}
-
-void vksk_RuntimeSteamIndicateAchievementProgress(WrenVM* vm) {
-	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
-
-}
-
+// setAchievement(name) -> bool
 void vksk_RuntimeSteamSetAchievement(WrenVM* vm) {
-	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
-
+	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_STRING, FOREIGN_END)
+	if (gSteamAPIInit) {
+		SteamUserStats()->SetAchievement(wrenGetSlotString(vm, 0));
+		wrenSetSlotBool(vm, 0, SteamUserStats()->StoreStats());
+	}
 }
 
+// activateGameOverlay(dialogue)
 void vksk_RuntimeSteamActivateGameOverlay(WrenVM* vm) {
-	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
-
+	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_STRING, FOREIGN_END)
+	if (gSteamAPIInit) {
+		SteamFriends()->ActivateGameOverlay(wrenGetSlotString(vm, 1));
+	}
 }
 
+// activateGameOverlayToWebpage(website)
 void vksk_RuntimeSteamActivateGameOverlayToWebPage(WrenVM* vm) {
-	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
-
+	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_STRING, FOREIGN_END)
+	if (gSteamAPIInit) {
+		SteamFriends()->ActivateGameOverlayToWebPage(wrenGetSlotString(vm, 1));
+	}
 }
 
+// getPersonaName()
 void vksk_RuntimeSteamGetPersonaName(WrenVM* vm) {
 	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
 	if (gSteamAPIInit)
 		wrenSetSlotString(vm, 0, SteamFriends()->GetPersonaName());
 	else
 		wrenSetSlotString(vm, 0, "");
+}
+
+// attemptStoreStats() -> bool
+void vksk_RuntimeSteamAttemptStoreStats(WrenVM* vm) {
+	VALIDATE_FOREIGN_ARGS(vm, FOREIGN_END)
+	if (gSteamAPIInit) {
+		wrenSetSlotBool(vm, 0, SteamUserStats()->StoreStats());
+	}
 }
--- src/SteamBindings.h
+++ src/SteamBindings.h
@@ -11,16 +11,16 @@ extern "C" {
 void vksk_SteamInit();
 void vksk_SteamQuit();
 
+void vksk_RuntimeSteamIsInitialized(WrenVM *vm);
 void vksk_RuntimeSteamGetAchievement(WrenVM *vm);
 void vksk_RuntimeSteamGetAchievementAchievedPercent(WrenVM *vm);
 void vksk_RuntimeSteamGetAchievementAndUnlockTime(WrenVM *vm);
-void vksk_RuntimeSteamGetAchievementDisplayAttribute(WrenVM *vm);
-void vksk_RuntimeSteamGetAchievementName(WrenVM *vm);
-void vksk_RuntimeSteamIndicateAchievementProgress(WrenVM *vm);
+void vksk_RuntimeSteamGetAchievementDisplayAttributes(WrenVM *vm);
 void vksk_RuntimeSteamSetAchievement(WrenVM *vm);
 void vksk_RuntimeSteamActivateGameOverlay(WrenVM *vm);
 void vksk_RuntimeSteamActivateGameOverlayToWebPage(WrenVM *vm);
 void vksk_RuntimeSteamGetPersonaName(WrenVM *vm);
+void vksk_RuntimeSteamAttemptStoreStats(WrenVM *vm);
 
 #ifdef __cplusplus
 }
